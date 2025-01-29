3b07c4866712c48bb290127ffafbc176a4cd52c8
Jordan Atanasovski <jordfocus@gmail.com>
2025-01-29 22:15:34 +0100

    potential answers update

--- kvizovi/math-quiz-division-by2-level-1/script.js
+++ kvizovi/math-quiz-division-by2-level-1/script.js
@@ -35,28 +35,11 @@ function nextQuestion() {
 }
 
 function getOptions() {
-
-    for (let i = 0; i < 4; i++ && i != ansOpt) {
-        
-        let generatedAnswer = Math.floor(Math.random() * 10);
-        for (let j = 0; j < 200; j++)
-        {
-            if (generatedAnswer != 0 
-                && generatedAnswer != answer)
-            {
-                break;
-            }
-
-            generatedAnswer = Math.floor(Math.random() * 10);
-
-
-        }
-        buttons[i].innerHTML = generatedAnswer;
-
-        if (answer < 0) {
-            buttons[i].innerHTML = "-" + buttons[i].innerHTML;
-        }
-    }
+    buttons[0].innerHTML = n1 - Math.floor(Math.random() * n1/2);
+    buttons[1].innerHTML = n1 - Math.floor(Math.random() * n1/3);
+    buttons[2].innerHTML = n1 - Math.floor(Math.random() * n1);
+    buttons[3].innerHTML = n1 - Math.floor(Math.random() * n1/4);
+   
     ansOpt = Math.floor(Math.random() * 4);
     buttons[ansOpt].innerHTML = answer;
 }
