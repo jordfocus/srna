3bcea7d588a9f50b277bf9c4ab2548a1b2a03675
Jordan Atanasovski <jordfocus@gmail.com>
2025-03-25 21:36:34 +0100

    new quizes

---
+++ kvizovi/math-quiz-division-by2-level-3/script.js
@@ -0,0 +1,47 @@
+function nextQuestion() {
+
+    progress.style.width = "100%";
+    timed();
+    // timed();
+    fScore.innerHTML = score.innerHTML;
+    if (qNo.innerText == "10") {
+        whenFinished();
+    }
+    n1 = Math.floor(Math.random() * 100);
+    n2 = 2;
+    opSelector = "/";
+
+    if (opSelector == "/") {
+        for (let i = 0; i < 200; i++) {
+            if (n1 % n2 == 0 
+                && n1 != 0 
+                && n1 != n2
+                && n1/n2 <= 250) {
+                break;
+            }
+            n1 = Math.floor(Math.random() * 500);
+            n2 = 2;
+        }
+    }
+
+    question.innerHTML = n1 + opSelector + n2;
+    answer = eval(question.innerHTML);
+    question.innerHTML = "Преполови го бројот " + n1;
+
+    // console.log("answer: " + answer);
+    getOptions();
+    getQNo();
+
+}
+
+function getOptions() {
+    let randValue = Math.floor(Math.random() * 2);
+
+    buttons[0].innerHTML = n1 - Math.floor(Math.random() * n1);
+    buttons[1].innerHTML = n1 - Math.floor(Math.random() * n1);
+    buttons[2].innerHTML = n1 - Math.floor(Math.random() * n1);
+    buttons[3].innerHTML = n1 - Math.floor(Math.random() * n1);
+   
+    ansOpt = Math.floor(Math.random() * 4);
+    buttons[ansOpt].innerHTML = answer;
+}
---
+++ kvizovi/math-quiz-multiplication-by-2-level-3/script.js
@@ -0,0 +1,49 @@
+function nextQuestion() {
+
+    progress.style.width = "100%";
+    timed();
+    // timed();
+    fScore.innerHTML = score.innerHTML;
+    if (qNo.innerText == "10") {
+        whenFinished();
+    }
+    n1 = Math.floor(Math.random() * 30);
+    n2 = 2;
+    opSelector = "*";
+
+    if (opSelector == "*") {
+        for (let i = 0; i < 100; i++) {
+            n1 = Math.floor(Math.random() * 250);
+            n2 = 2;
+
+            if (n1 * n2 <= 500
+                && n1 > 1
+                && n1 < 250) {
+                break;
+            }
+        }
+    }
+
+    question.innerHTML = n1 + opSelector + n2;
+    answer = eval(question.innerHTML);
+    question.innerHTML = "Удвој го бројот " + n1;
+
+    // console.log("answer: " + answer);
+    getOptions();
+    getQNo();
+
+}
+
+function getOptions() {
+    let randValue = Math.floor(Math.random() * 2);
+    buttons[0].innerHTML = randValue == 0 && answer > n1 ? answer - n1 : answer + n1;
+    randValue = Math.floor(Math.random() * 2);
+    buttons[1].innerHTML = randValue == 0 && answer > n2 ? answer - n2 : answer + n2;
+    randValue = Math.floor(Math.random() * 2);
+    buttons[2].innerHTML = randValue == 0 && answer > 2*n1 ? answer - 2*n1 : answer + 2*n1;
+    randValue = Math.floor(Math.random() * 2);
+    buttons[3].innerHTML = randValue == 0 && answer > 2*n2 ? answer - 2*n2 : answer + 2*n2;
+
+    ansOpt = Math.floor(Math.random() * 4);
+    buttons[ansOpt].innerHTML = answer;
+}
